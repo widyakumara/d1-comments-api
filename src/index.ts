00c179fce4a35d1d1ee5567de98d32e51fd24d63
Dewa Widyakumara <widyakumara@gmail.com>
2026-05-22 15:28:29 +0700

    feat: endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { Hono } from "hono";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/", (c) => {
   return c.text("Hello World!");
+});
+
+app.get("/api/posts/:slug/comments", async (ctx) => {
+  // Do something and return an HTTP response
+  // Optionally, do something with c.req.param("slug")
+  const params = ctx.req.param();
+  return ctx.json(params);
+});
+
+app.post("/api/posts/:slug/comments", async (ctx) => {
+  // Do something and return an HTTP response
+  // Optionally, do something with c.req.param("slug")
+  const params = ctx.req.param();
+  return ctx.json(params);
 });
 
 export default app;
