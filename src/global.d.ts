4513672f8dff597fec43a6de3e0cfc902ce582b1
Dewa Widyakumara <widyakumara@users.noreply.github.com>
2026-05-22 15:48:51 +0700

    feat: queries (#7)

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,8 @@
 type Bindings = {
   DB_BINDING: D1Database;
 };
+
+type CommentType = {
+  author: string;
+  body: string;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,46 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono<{ Bindings: Bindings }>();
+app.use("/api/*", cors());
 
 app.get("/", (c) => {
   return c.text("Hello World!");
 });
 
-app.get("/api/posts/:slug/comments", async (ctx) => {
-  // Do something and return an HTTP response
-  // Optionally, do something with c.req.param("slug")
-  const params = ctx.req.param();
-  return ctx.json(params);
+app.get("/api/posts/:slug/comments", async (ctx: Context) => {
+  const query = "SELECT * FROM comments WHERE post_slug = ?";
+
+  const { slug } = ctx.req.param();
+  const { results } = await ctx.env.DB_BINDING.prepare(query).bind(slug).run();
+
+  return ctx.json(results);
 });
 
 app.post("/api/posts/:slug/comments", async (ctx) => {
-  // Do something and return an HTTP response
-  // Optionally, do something with c.req.param("slug")
-  const params = ctx.req.param();
-  return ctx.json(params);
+  const { slug } = ctx.req.param();
+  const { author, body } = await ctx.req.json<CommentType>();
+
+  if (!author) {
+    return ctx.json({ code: 400, message: "Missing author" }, 400);
+  }
+
+  if (!body) {
+    return ctx.json({ code: 400, message: "Missing body" }, 400);
+  }
+
+  const query =
+    "INSERT INTO comments (author, body, post_slug) VALUES (?, ?, ?)";
+
+  const { success } = await ctx.env.DB_BINDING.prepare(query)
+    .bind(author, body, slug)
+    .run();
+
+  if (success) {
+    return ctx.json({ code: 201, message: "Created" }, 201);
+  } else {
+    return ctx.json({ code: 500, message: "Something went wrong" }, 500);
+  }
 });
 
 export default app;
