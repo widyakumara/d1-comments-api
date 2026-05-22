8b93a9ec85235ed0c6a1080e4a50ceebc563aba1
Dewa Widyakumara <widyakumara@users.noreply.github.com>
2026-05-22 15:01:12 +0700

    chore: prettier (#2)

---
+++ prettier.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import("prettier").Config} */
+
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  endOfLine: "lf",
+  singleAttributePerLine: false,
+  plugins: [],
+  overrides: [{ files: "*.json", options: { trailingComma: "none" } }],
+};
